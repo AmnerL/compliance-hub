5ee1c26544a7bc8afed5285a87a939d1b853101b
Amner LS <amnerlevi20@gmail.com>
2026-05-27 11:31:00 -0500

    chore: initialize spring boot project with maven configuration

---
+++ src/test/java/com/portafolio/compliancehub/ComplianceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.compliancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplianceHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
