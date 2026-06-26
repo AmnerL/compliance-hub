6314218e15d3d72709fd47b78fe001190974269b
Amner LS <amnerlevi20@gmail.com>
2026-06-26 14:57:51 -0500

    chore: add thymeleaf & flying-saucer dependencies, update .gitignore and checklist repository findByRegulationId query

--- src/main/java/com/portafolio/compliancehub/checklist/infrastructure/persistence/jpa/repositories/ChecklistRepository.java
+++ src/main/java/com/portafolio/compliancehub/checklist/infrastructure/persistence/jpa/repositories/ChecklistRepository.java
@@ -1,6 +1,7 @@
 package com.portafolio.compliancehub.checklist.infrastructure.persistence.jpa.repositories;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import com.portafolio.compliancehub.checklist.domain.model.aggregates.Checklist;
 @Repository
 public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
     long countByCreatedAtAfter(LocalDateTime dateTime);
+    Optional<Checklist> findByRegulationId(Long regulationId);
 }
